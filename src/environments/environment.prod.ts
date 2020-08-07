427763d6b23f7dbd79be26b964710e2f36ed606e
Exequiel Schmidt <exee.schmidt@gmail.com>
2020-08-06 22:54:52 -0300

    Fix environment producction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+const urlBase = 'https://angular-portafolio-69110.firebaseio.com';
+
 export const environment = {
-  production: true
+  production: true,
+  urlDataPage: 'assets/data/data-page.json',
+  urlIProductsIdx: urlBase + '/productos_idx.json',
+  urlTeam: urlBase + '/equipo.json'
 };
